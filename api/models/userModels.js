86b6f91e7f377b6d48be0d7a52ad03cf0dbcc2cb
Amanda Phillips <amanda@phillipsdev.org>
2018-03-23 10:43:48 -0500

    Schema set up

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 11;
@@ -8,9 +8,19 @@ const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre("save", function(next) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
@@ -27,4 +37,4 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
 // if you're really stuck with this at this point, you can reference this document.
 // https://github.com/LambdaSchool/Auth-JWT/blob/master/models/index.js This is what we're going for here.
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
